3508bdb10918be5722e05bb4cf456d9681da9800
Boontwiks <boontwiks@gmail.com>
2017-07-09 15:32:15 +0200

    Simple server, some page design
    The first steps to building the site.
    Using SASS for CSS, handlebars for easy templating and maximum
    	reusability.
    The site is nowhere near complete in its current stage, but presents
    	some initial ideas for further exploration.
    
    Issues or concerns:
     - CSS Animation for changing text content is not working. Since the
    	event is triggered on reflow, we'll probably need to do this
    	with js and some placeholder elements.

---
+++ app/index.js
@@ -0,0 +1,75 @@
+/*
+ * About this file:
+ * This file hosts the server for the main web site.
+ *
+ */
+
+// Quick constants:
+const _port = 33333
+const _globals = require('./globals.json')
+
+// CODE
+
+// Requires
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const app = express()
+const sass = require('node-sass')
+const handlebars = require('handlebars')
+const readline = require('readline');
+
+// Load-Time Constants and Templates
+var template_home;
+function compile_handlebars() {
+	template_home = handlebars.compile(fs.readFileSync(path.join(__dirname, 'templates/home.html')).toString())
+}
+
+function compile_sass() {
+	// TODO: Easily compule the entire directory
+	sass.render({
+		file: path.join(__dirname, 'dev/styles/home.sass'),
+		outFile: path.join(__dirname, 'public/styles/home.css')
+	}, (err, result) => {
+		if (err)
+			return console.error('Sass encountered an error:', err)
+		fs.writeFile(path.join(__dirname, 'public/styles/home.css'), result.css, (err) => {
+			if (err)
+				return console.error('Error writing compiled css')
+		})
+	})
+}
+
+compile_handlebars()
+compile_sass()
+
+// Application
+app.get('/', (req, res) => {
+	var data = {'page': _globals.pages.general}
+	res.send(template_home(data))
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(_port, (err) => {
+	if (err)
+		return console.error('Something bad happened with the server:', err)
+	console.log('Express Server listening on Port ' + _port)
+	console.log("Press 'r' to recompile html and css")
+})
+
+// Keyboard input
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true)
+process.stdin.on('keypress', (chunk, key) => {
+	if(key.sequence === '\u0003') {
+        process.exit();
+    }
+	if (key && key.name == 'r') {
+		process.stdout.write('Recompiling templates and sass...')
+		compile_handlebars()
+		compile_sass()
+		process.stdout.write('\r                                 \rDone.')
+		setTimeout(() => {process.stdout.write('\r     \r')}, 500) //fancy! we don't want to clog the console.
+	}
+})
---
+++ app/public/scripts/home.js
@@ -0,0 +1,14 @@
+// IBlity, the ... IB textbook
+var about_texts = ['completely free', 'community-driven', 'open-source', 'up-to-date']
+var about_interval = 3000
+var about_index = 0
+
+function changeAbout() {
+	if (about_index == about_texts.length - 1) about_index = 0
+	else about_index += 1
+
+	$("#body--dark--about--span-middle").html(about_texts[about_index])
+	setTimeout(changeAbout, about_interval)
+}
+
+setTimeout(changeAbout, about_interval)
